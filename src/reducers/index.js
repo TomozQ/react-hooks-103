df2649935ab00027a6ee0b18450eedf521ff770d
TomozQ <inf.4clover@gmail.com>
2020-12-23 20:59:58 +0900

    create Context

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,13 +1,17 @@
 import React from 'react'
+import {
+    SHIPMENT_ITEM,
+    INSTOCK_ITEM,
+} from '../actions/index'
 
 const items = ( state=[], action ) => {
     switch( action.type ){
-        case 'SHIPMENT_ITEM':
+        case SHIPMENT_ITEM:
             const item = { item: action.item }
             const length = state.length
             const id = length === 0 ? 1 : state[length - 1].id + 1
             return [ ...state, { id, ...item }]
-        case 'INSTOCK_ITEM':
+        case INSTOCK_ITEM:
             return state
         default:
             return state
